ed3f009f3c6c630297352235dd26b9bbb7ab3943
Sanket <panchmind1@gmail.com>
2025-03-18 16:28:18 +0530

    add redirect to register route in app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegisterloginformComponent } from './registerloginform/registerloginform.component';
 
 const routes: Routes = [
+  {path:'', redirectTo:'register', pathMatch:'full'},
   {path: 'register', component: RegisterloginformComponent},
   { path: '', loadChildren: () => import('./layout/secure/secure.module').then(m => m.SecureModule) },
 ];
